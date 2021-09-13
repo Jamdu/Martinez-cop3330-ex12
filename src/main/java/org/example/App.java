d40e5053ee3366d6fd68df06b5bb7878cdac963c
Jamdu <Martinez.josh506@gmail.com>
2021-09-12 22:29:02 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+     Scanner scnr = new Scanner(System.in);
+
+     System.out.println("Enter the principal:");
+     int principal = scnr.nextInt();
+
+     System.out.println("Enter the rate of interest");
+     double rateOfInterest = scnr.nextDouble();
+
+     System.out.println("Enter the number of years");
+     int years = scnr.nextInt();
+
+
+     double A = principal * (1 + (rateOfInterest / 100) * years);
+
+     System.out.format("After %d years at %.1f%%, the investment will be worth $%.2f.", years, rateOfInterest, A);
+
+    }
+}
